439b8a16447419fbba89f1c08fb964e35c76aec4
NibirBiswas <98407737+NibirBiswas@users.noreply.github.com>
2023-06-27 00:30:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,13 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
-import os
 
 def fetch_poster(id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=b22cb560447294856a2dba69bc7b10a0&language=en-US".format(id)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    full_path = "https://image.tmdb.org/t/p/original/" + poster_path
     return full_path
 
 def recommend(movie):
@@ -23,27 +22,12 @@ def recommend(movie):
         recommended_movie_posters.append(fetch_poster(id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
-    return recommended_movie_names, recommended_movie_posters
+    return recommended_movie_names,recommended_movie_posters
 
 st.header('MovieMatch - a Project by Nibir Biswas')
 movies_dict = pickle.load(open('movies.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-
-# Define the URL and local file path for 'similarity.pkl'
-similarity_url = "https://drive.google.com/file/d/1Ep1VXBos5Nqo43OPxwk7TVXEfM-x9ulH/view?usp=drive_link"
-similarity_file_path = "similarity.pkl"
-
-def download_file(url, file_path):
-    response = requests.get(url)
-    with open(file_path, 'wb') as f:
-        f.write(response.content)
-
-# Download the file if it doesn't exist locally
-if not os.path.exists(similarity_file_path):
-    download_file(similarity_url, similarity_file_path)
-
-# Load the 'similarity.pkl' file
-similarity = pickle.load(open(similarity_file_path, 'rb'))
+similarity = pickle.load(open('https://drive.google.com/file/d/1Ep1VXBos5Nqo43OPxwk7TVXEfM-x9ulH/view?usp=drive_link', 'rb'))
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
@@ -51,7 +35,7 @@ selected_movie = st.selectbox(
     movie_list
 )
 if st.button('Recommend Me!'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(recommended_movie_names[0])
